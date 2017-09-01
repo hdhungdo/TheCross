031986ce2ec15d9e22cd1e5485914fe7669cf34e
hdo33 <hdo33@gatech.edu>
2017-09-01 00:04:12 -0400

    clean up

--- src/pages/initial/initial.ts
+++ src/pages/initial/initial.ts
@@ -17,7 +17,7 @@ import { BibleServiceProvider } from "../../providers/bible-service/bible-servic
 })
 export class InitialPage {
   splash:boolean = true;
-  rootPage:string = 'BiblePage';
+  rootPage:string = 'MenuPage';
 
   constructor(private bibleService: BibleServiceProvider, public storage: Storage,
               public navCtrl: NavController) {
@@ -29,7 +29,6 @@ export class InitialPage {
     this.getFontSize();
     setTimeout(() => {
       this.splash = false;
-      this.navCtrl.setRoot(this.rootPage);
     }, 2000);
   }
 
